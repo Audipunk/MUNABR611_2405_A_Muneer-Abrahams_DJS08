a49b057604c26092962c10d39f79ca9c73f251e9
Muneer Abrahams <audipunkwp@gmail.com>
2025-03-08 01:49:15 +0200

    Create login.jsx

---
+++ pages/login.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import { useState } from "react";
+import { loginUser } from "../../api";
+
+// Makes sure our page inputs are empty after initial load in
+export default function Login() {
+  const [loginFormData, setLoginFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const [status, setStatus] = useState("idle");
+  const [error, setError] = useState(null);
+  const location = useLocation();
+
+  const navigate = useNavigate();
+
+  //   Dealing with submit credentials
+  function handleSubmit(e) {
+    e.preventDefault();
+    setStatus("submitting");
+    loginUser(loginFormData)
+      .then((data) => {
+        console.log(data);
+        setError(null);
+        navigate("/host");
+        console.log("I am, navigating!");
+      })
+      //   Checking for any errors in inputs
+      .catch((err) => {
+        setError(err);
+        localStorage.setItem("loggedin", true);
+      })
+      .finally(() => {
+        setStatus("idle");
+      });
+  }
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setLoginFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  }
+
+  //   Displaying form elements
+  return (
+    <div className="login-container">
+      {location.state?.message && (
+        <h3 className="login-error">{location.state.message}</h3>
+      )}
+      <h1>Sign in to your account</h1>
+      {error?.message && <h3 className="login-error">{error.message}</h3>}
+      <form onSubmit={handleSubmit} className="login-form">
+        <input
+          name="email"
+          onChange={handleChange}
+          type="email"
+          placeholder="Email address"
+          value={loginFormData.email}
+        />
+        <input
+          name="password"
+          onChange={handleChange}
+          type="password"
+          placeholder="Password"
+          value={loginFormData.password}
+        />
+        <button disabled={status === "submitting"}>
+          {status === "submitting" ? "Logging in..." : "Log in"}
+        </button>
+      </form>
+    </div>
+  );
+}
